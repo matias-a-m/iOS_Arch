3de596c24691ebf0755399b95f1e81d7745abd72
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2024-02-07 20:22:25 -0300

    added MVC-C

--- MVC_Architecture/MVC_Architecture/LIST OF CHARACTERS/CONTROLLER/ListOfCharactersTableViewDelegate.swift
+++ MVC_Architecture/MVC_Architecture/LIST OF CHARACTERS/CONTROLLER/ListOfCharactersTableViewDelegate.swift
@@ -1,15 +1,32 @@
 import Foundation
 import UIKit
 
-class ListOfCharactersTableViewDelegate: NSObject, UITableViewDelegate{
+// MARK: - ListOfCharactersTableViewDelegate
+
+/// Clase que actúa como delegado de la tabla para gestionar la selección de celdas y la altura de las mismas.
+class ListOfCharactersTableViewDelegate: NSObject, UITableViewDelegate {
     
-    var didTapOnCell:((Int) -> Void)?
+    // MARK: - Properties
     
+    /// Closure que se llama cuando se toca una celda en la tabla. El parámetro representa el índice de la celda seleccionada.
+    var didTapOnCell: ((Int) -> Void)?
+    
+    // MARK: - UITableViewDelegate Methods
+    
+    /// Método llamado cuando se selecciona una celda en la tabla.
+    /// - Parameters:
+    ///   - tableView: La tabla que contiene la celda.
+    ///   - indexPath: La ubicación de la celda seleccionada.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         didTapOnCell?(indexPath.row)
     }
     
+    /// Método que especifica la altura de una celda en la tabla.
+    /// - Parameters:
+    ///   - tableView: La tabla que contiene la celda.
+    ///   - indexPath: La ubicación de la celda.
+    /// - Returns: La altura deseada para la celda en la posición especificada.
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        140
+        return 140
     }
 }
